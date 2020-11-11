0d3fa697fb802e5e892ca09a08e903d4a3f6eb04
RAJNEESH KHARE <40913547+Rajnishkhare31428@users.noreply.github.com>
2020-11-11 22:40:57 +0530

    various components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    RegisterFormComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
